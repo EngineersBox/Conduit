862da63d1c1f108dcc125f75667c415a9e4a770d
u6940136 <u6940136@anu.edu.au>
2023-01-25 20:39:37 +1100

    Fixed dependencies in pom

---
+++ src/main/java/com/engineersbox/conduit/schema/MetricsSchema.java
@@ -0,0 +1,11 @@
+package com.engineersbox.conduit.schema;
+
+import java.util.Map;
+
+public class MetricsSchema {
+
+    private final Map<String, Pair<String, String>> paths;
+
+    private MetricsSchema() {}
+
+}
