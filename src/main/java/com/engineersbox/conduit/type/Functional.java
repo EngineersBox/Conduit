f7653f60cb388bb9484919a45c2f19e2c28087eb
u6940136 <u6940136@anu.edu.au>
2023-02-12 21:14:31 +1100

    Removed duplicate throws adapters

--- src/main/java/com/engineersbox/conduit/type/Functional.java
+++ src/main/java/com/engineersbox/conduit/type/Functional.java
@@ -1,20 +1,47 @@
 package com.engineersbox.conduit.type;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Functional {
 
-    @FunctionalInterface
-    public interface ThrowsConsumer<T> {
-
-        void accept(final T value) throws Exception;
-
+    interface ThrowsFunction<T, R> {
+        R apply(final T t) throws Exception;
     }
 
-    public static <T> Consumer<T> uncheckedConsumer(final ThrowsConsumer<T> consumer) {
-        return (final T value) -> {
+    interface ThrowsConsumer<T> {
+        void accept(final T t) throws Exception;
+    }
+
+    interface ThrowsBiConsumer<T, U> {
+        void accept(final T t, final U u) throws Exception;
+    }
+
+    static <T, R> Function<T, R> uncheckedFunction(final ThrowsFunction<T, R> function) {
+        return (final T t) -> {
             try {
-                consumer.accept(value);
+                return function.apply(t);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    static <T> Consumer<T> uncheckedConsumer(final ThrowsConsumer<T> consumer) {
+        return (final T t) -> {
+            try {
+                consumer.accept(t);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    static <T, U> BiConsumer<T, U> uncheckedBiConsumer(final ThrowsBiConsumer<T, U> consumer) {
+        return (final T t, final U u) -> {
+            try {
+                consumer.accept(t, u);
             } catch (final Exception e) {
                 throw new RuntimeException(e);
             }
