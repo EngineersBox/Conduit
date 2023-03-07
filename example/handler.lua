7e6420003764e16f55d92f07ef8cea43d5694f8a
u6940136 <u6940136@anu.edu.au>
2023-03-08 00:03:54 +1100

    Fixed lua table handling for handler method arguments

--- example/handler.lua
+++ example/handler.lua
@@ -1,14 +1,20 @@
 -- Provided conduit package for performing contextual operations
-local conduit = require("conduit");
+--local conduit = require("conduit");
 
 test_handler = {};
 
+test_handler.other = "value";
+
 -- Determine if the service is of an appropriate version to collect from
-function test_handler.shouldCollectTestMetric(ctx)
+function test_handler.shouldCollectTestMetric(self, ctx)
+    for k,v in pairs(ctx) do
+        print("Key: "..k.." Value: "..type(v));
+    end
     local serviceVersion = ctx["service_version"];
+    print(serviceVersion);
     local serviceVersionType = type(serviceVersion);
     if not (serviceVersionType == "number") then
-        error("Service version is not of type 'number', got", serviceVersionType);
+        error("Service version is not of type 'number', got " .. serviceVersionType, 1);
     end
     ctx.executionContext.shouldRun = serviceVersion > 2;
     return ctx;
