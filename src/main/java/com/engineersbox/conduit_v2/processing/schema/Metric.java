fc7c18604c4d3d8a0bd669934004ae84f5acdb15
u6940136 <u6940136@anu.edu.au>
2023-04-27 22:34:20 +1000

    Implemented skeleton method structuring of retrieval section

--- src/main/java/com/engineersbox/conduit_v2/processing/schema/Metric.java
+++ src/main/java/com/engineersbox/conduit_v2/processing/schema/Metric.java
@@ -5,15 +5,19 @@ import com.engineersbox.conduit.schema.metric.ParameterizedMetricType;
 public class Metric {
 
     // TODO: Implement this
+    private String path;
+    private String metricNamespace;
+    private String handlerMethod;
+    private ParameterizedMetricType type;
 
     public String getPath() {
         // TODO: Implement this
-        return null;
+        return this.path;
     }
 
     public ParameterizedMetricType getType() {
         // TODO: Implement this
-        return null;
+        return this.type;
     }
 
 }
