3ebe89cc26332eb386e77a30a74470c76d232539
u6940136 <u6940136@anu.edu.au>
2023-04-30 18:18:39 +1000

    Added handling stream in pipeline

--- src/main/java/com/engineersbox/conduit_v2/processing/pipeline/Pipeline.java
+++ src/main/java/com/engineersbox/conduit_v2/processing/pipeline/Pipeline.java
@@ -2,13 +2,20 @@ package com.engineersbox.conduit_v2.processing.pipeline;
 
 import com.engineersbox.conduit.schema.MetricsSchemaProvider;
 import com.engineersbox.conduit_v2.processing.schema.Metric;
+import com.engineersbox.conduit_v2.processing.task.MetricProcessingTask;
 import com.engineersbox.conduit_v2.processing.task.TaskExecutorPool;
 import com.engineersbox.conduit_v2.retrieval.content.ContentManager;
+import com.engineersbox.conduit_v2.retrieval.content.RetrievalHandler;
 import io.riemann.riemann.Proto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Pipeline {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Pipeline.class);
 
     private final MetricsSchemaProvider schemaProvider = null;
     private final ContentManager<?, ?, ?, ?> contentManager = null;
@@ -20,9 +27,27 @@ public class Pipeline {
         this.executor = new TaskExecutorPool(() -> null);
     }
 
-    private List<Proto.Event> handleMetric(final Metric metric) {
-        final Object value = this.contentManager.retrieveMetricValue(metric);
-        return this.transformer.transform(metric, value);
+    public void execute() {
+        final AtomicReference<RetrievalHandler<Metric>> retrieverReference = new AtomicReference<>(this.contentManager);
+        // Update metric to be correct type in MetricSchema definition
+        this.schemaProvider.provide().forEach((final String key, final Object metric) -> {
+            LOGGER.debug("Submitting metric for parsing: {}", key);
+            handleMetric(
+                    (Metric) metric,
+                    retrieverReference
+            );
+        });
+        this.schemaProvider.refresh();
+    }
+
+    private void handleMetric(final Metric metric,
+                              final AtomicReference<RetrievalHandler<Metric>> retriever) {
+        final Proto.Event eventTemplate = null; // Get from schema
+        this.executor.submit(new MetricProcessingTask(
+                metric,
+                eventTemplate,
+                retriever
+        ));
     }
 
 }
