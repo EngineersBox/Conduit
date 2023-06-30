09d8710994e6388f989f292198ab9091e6dea8e2
u6940136 <u6940136@anu.edu.au>
2023-06-30 22:16:28 +1000

    Added missing provider parsing invocation for raw string json before forwarding as object

--- src/main/java/com/engineersbox/conduit_v2/retrieval/path/PathTraversalHandler.java
+++ src/main/java/com/engineersbox/conduit_v2/retrieval/path/PathTraversalHandler.java
@@ -1,17 +1,14 @@
 package com.engineersbox.conduit_v2.retrieval.path;
 
 import com.jayway.jsonpath.*;
-import com.jayway.jsonpath.spi.json.JsonProvider;
 
 public class PathTraversalHandler<R> {
 
     private final ParseContext parseContext;
-    private final JsonProvider provider;
     private ReadContext context;
 
     public PathTraversalHandler(final Configuration configuration) {
         this.parseContext = JsonPath.using(configuration);
-        this.provider = configuration.jsonProvider();
     }
 
     public void saturate(final R raw) {
