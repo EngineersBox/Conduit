35bf93c39611c6db381df24cb233a9541f3816f4
EngineersBox <toastcraft.info@gmail.com>
2023-07-19 22:33:43 +1000

    Expanded PathTraversalHandler to additionally handle saturation for InputStream and byte[]

--- src/main/java/com/engineersbox/conduit_v2/retrieval/path/PathTraversalHandler.java
+++ src/main/java/com/engineersbox/conduit_v2/retrieval/path/PathTraversalHandler.java
@@ -2,6 +2,8 @@ package com.engineersbox.conduit_v2.retrieval.path;
 
 import com.jayway.jsonpath.*;
 
+import java.io.InputStream;
+
 public class PathTraversalHandler<R> {
 
     private final ParseContext parseContext;
@@ -12,9 +14,15 @@ public class PathTraversalHandler<R> {
     }
 
     public void saturate(final R raw) {
-        this.context = raw instanceof String rawString
-                ? this.parseContext.parse(rawString)
-                : this.parseContext.parse(raw);
+        if (raw instanceof String rawString) {
+            this.parseContext.parse(rawString);
+        } else if (raw instanceof InputStream rawInputStream) {
+            this.parseContext.parse(rawInputStream);
+        } else if (raw instanceof byte[] rawBytes) {
+            this.parseContext.parse(rawBytes);
+        } else {
+            this.parseContext.parse(raw);
+        }
     }
 
     public <T> T read(final String path,
