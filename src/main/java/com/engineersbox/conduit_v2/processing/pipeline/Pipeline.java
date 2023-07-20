1c3902aa8a4664a818901b6292bca3e94c811de2
EngineersBox <toastcraft.info@gmail.com>
2023-07-20 22:04:13 +1000

    Added todo for structural refactor of pipeline

--- src/main/java/com/engineersbox/conduit_v2/processing/pipeline/Pipeline.java
+++ src/main/java/com/engineersbox/conduit_v2/processing/pipeline/Pipeline.java
@@ -17,6 +17,15 @@ import java.util.stream.StreamSupport;
 
 public class Pipeline<T> implements Consumer<T> {
 
+    /* TODO: Refactor pipeline to use ExMesh structure of tasks as stages with graph mappings.
+     *       Also support the delegation to a worker pool for individual pipelines based on a
+     *       pool that is either provided or defaulted to a single thread (the current thread).
+     *       Allow for pipeline stages to be dynamically created only when needed to avoid
+     *       all stages living as long as the pipeline. Releasing of stages and their resources
+     *       should be done as soon as all forward dependencies on the stage are satisfied in
+     *       terms of execution and result storage/handling.
+     */
+
     private static final Logger LOGGER = LoggerFactory.getLogger(Pipeline.class);
 
     private final Map<String, Object> context;
