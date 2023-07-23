a3a371c8d7b8315ade9ee63b0ac2102af8305175
EngineersBox <toastcraft.info@gmail.com>
2023-07-24 01:15:44 +1000

    Added TODO note for improving metric type deserialisation

--- src/main/java/com/engineersbox/conduit_v2/processing/schema/ParameterizedMetricType.java
+++ src/main/java/com/engineersbox/conduit_v2/processing/schema/ParameterizedMetricType.java
@@ -1,4 +1,13 @@
 package com.engineersbox.conduit_v2.processing.schema;
 
 public class ParameterizedMetricType {
+
+    /* TODO: This represents either a primitive type or a
+     *       container type, depending on the "type" field value.
+     *       This should be a base class that is extended as two
+     *       child classes, one for primitive types and another
+     *       for container types. This should be deserialisable
+     *       by using the @JsonTypeInfo and @JsonTypeIdResolver
+     */
+
 }
