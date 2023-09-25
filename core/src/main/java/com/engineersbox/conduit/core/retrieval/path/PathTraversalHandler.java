a84b87bc214bc8b0cd0a964fe91290c5fd0895d8
EngineersBox <toastcraft.info@gmail.com>
2023-09-26 01:14:28 +1000

    Refactored to support affinity bound JsonPath config, defaulting to schema specific config with default provider

--- core/src/main/java/com/engineersbox/conduit/core/retrieval/path/PathTraversalHandler.java
+++ core/src/main/java/com/engineersbox/conduit/core/retrieval/path/PathTraversalHandler.java
@@ -3,17 +3,13 @@ package com.engineersbox.conduit.core.retrieval.path;
 import com.engineersbox.conduit.core.processing.task.worker.ClientBoundForkJoinWorkerThead;
 import com.engineersbox.conduit.core.retrieval.configuration.AffinityBoundConfigProvider;
 import com.jayway.jsonpath.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class PathTraversalHandler<R> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PathTraversalHandler.class);
     private ReadContext context;
+    private ParseContext parseContext;
     private Configuration config;
     private final boolean cachedConfig;
 
@@ -23,6 +19,7 @@ public class PathTraversalHandler<R> {
         this.config = config;
         this.cachedConfig = false;
         this.affinityBoundConfig = false;
+        this.parseContext = JsonPath.using(config);
     }
 
     public PathTraversalHandler(final boolean cachedConfig) {
@@ -36,18 +33,20 @@ public class PathTraversalHandler<R> {
                     ClientBoundForkJoinWorkerThead.getThreadAffinityId()
             );
         }
-        if (raw instanceof String rawString) {
-            this.context = JsonPath.parse(rawString, config);
-        } else if (raw instanceof InputStream rawInputStream) {
-            this.context = JsonPath.parse(rawInputStream, config);
-        } else if (raw instanceof byte[] rawBytes) {
-            try (final ByteArrayInputStream bais = new ByteArrayInputStream(rawBytes)) {
-                this.context = JsonPath.parse(bais, config);
-            } catch (final IOException e) {
-                LOGGER.error("Unable to parse raw bytes into JsonPath instance", e);
-            }
+        final ParseContext ctx;
+        if (this.affinityBoundConfig) {
+            ctx = JsonPath.using(this.config);
         } else {
-            this.context = JsonPath.parse(raw, config);
+            ctx = this.parseContext;
+        }
+        if (raw instanceof String rawString) {
+            this.context = ctx.parse(rawString);
+        } else if (raw instanceof InputStream rawInputStream) {
+            this.context = ctx.parse(rawInputStream);
+        } else if (raw instanceof byte[] rawBytes) {
+            this.context = ctx.parseUtf8(rawBytes);
+        } else {
+            this.context = ctx.parse(raw);
         }
     }
 
